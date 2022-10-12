0c0ed2469e688829a92b63d2e0d3c4ff0dc25e48
Ashraful Islam <ashrafulislam21211@gmail.com>
2022-10-12 19:35:04 +0600

    Statistics  done

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,11 +1,38 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const Statistics = () => {
   const quizDatas = useLoaderData();
   const { data } = quizDatas;
   console.log(data);
-  return <div>This Statistic</div>;
+  const renderLineChart = (
+    <LineChart width={600} height={300} data={data} className="mx-auto mt-10">
+      <Line type="monotone" dataKey="total" stroke="#8884d8" />
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+    </LineChart>
+  );
+  return (
+    <div>
+      {renderLineChart}
+      <h1
+        className="text-center
+      "
+      >
+        This Chart is about Quiz Total{" "}
+      </h1>
+    </div>
+  );
 };
 
 export default Statistics;
